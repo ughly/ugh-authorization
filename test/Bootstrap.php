1768d2f556fa8dc487b073f2c496f3a34a116b65
Yawman, Chris <christopher.yawman@neustar.biz>
2014-09-17 14:03:29 -0400

    Fix php 5.3.3 error

--- test/Bootstrap.php
+++ test/Bootstrap.php
@@ -2,10 +2,10 @@
 
 chdir(__DIR__);
 
-$files = [
+$files = array(
     __DIR__ . '/../vendor/autoload.php',
     __DIR__ . '/../../../autoload.php'
-];
+);
 
 foreach ($files as $file) {
     if (file_exists($file)) {
