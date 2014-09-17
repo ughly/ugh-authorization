918150032c0e52f542d77e8efc98f298b9a2aa6c
Yawman, Chris <christopher.yawman@neustar.biz>
2014-09-17 11:02:22 -0400

    Search for vendor autoloader

--- test/Bootstrap.php
+++ test/Bootstrap.php
@@ -1,10 +1,20 @@
 <?php
 
+$files = [
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../../autoload.php'
+];
 
-
-// Composer autoloading
-if (file_exists('./vendor/autoload.php')) {
-    $loader = include './vendor/autoload.php';
+foreach ($files as $file) {
+    if (file_exists($file)) {
+        $loader = require $file;
+        break;
+    }
 }
+
+if (! isset($loader)) {
+    throw new RuntimeException('vendor/autoload.php could not be found. Did you install via composer?');
+}
+
 $loader->add('UghAuthorizationTest', "./test/UghAuthorizationTest");
 Zend\Mvc\Application::init(require './test/config/application.config.php');
