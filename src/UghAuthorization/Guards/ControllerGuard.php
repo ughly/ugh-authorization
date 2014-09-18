b8a6c346164e27f9a965d9e2ce5dd0102ea5daa3
commiejim <amajot@gmail.com>
2014-09-17 20:18:55 -0400

    edited more line endings and PSR-2 stuff

--- src/UghAuthorization/Guards/ControllerGuard.php
+++ src/UghAuthorization/Guards/ControllerGuard.php
@@ -40,5 +40,4 @@ class ControllerGuard implements Guard
 
         return $allowedRoles;
     }
-
 }
--- src/UghAuthorization/Guards/RouteGuardListener.php
+++ src/UghAuthorization/Guards/RouteGuardListener.php
@@ -17,5 +17,4 @@ class RouteGuardListener extends GuardListener
 
         $this->triggerForbiddenEvent($event);
     }
-
 }
