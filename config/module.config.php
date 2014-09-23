936e06a2760cb254906c41add5019135abb5ed4e
Yawman, Chris <christopher.yawman@neustar.biz>
2014-09-23 09:19:31 -0400

    Add Rbac factory to service manager config

--- config/module.config.php
+++ config/module.config.php
@@ -3,7 +3,8 @@
 return array(
     'services' => array(
         'factories' => array(
-            'UghAuthorization\Options\ModuleOptions' => 'UghAuthorization\Factory\Options\ModuleOptionsFactory'
+            'UghAuthorization\Options\ModuleOptions' => 'UghAuthorization\Factory\Options\ModuleOptionsFactory',
+            'UghAuthorization\Permissions\Rbac\Rbac' => 'UghAuthorization\Factory\Permissions\Rbac\RbacFactory'
         )
     )
 );
